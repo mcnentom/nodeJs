ae1a574457e73d42ec4074584faa3349a5d71c9f
mcnentom <tommymcnen016@gmail.com>
2024-02-19 22:15:31 +0300

    modified some files

--- Day1/index.js
+++ Day1/index.js
@@ -1,9 +1,23 @@
 const data = require('http');
-const received = require('./db.json');
-const requestListener = (req, res) =>{
-    JSON.stringify(received)
+// const received = require('./db.json');
+// const requestListener = (req, res) =>{
+//     JSON.stringify(received)
 
-}
-const required = data.createServer(requestListener);
+// }
+// const required = data.createServer(requestListener);
+// const Port = 3000;
+// required.listen(Port,'localhost',()=>console.log(`The server is running on ${Port}`));
+const fs = require('fs');
+const myserver = data.createServer((req, res)=>{
+    fs.readFile('db.json',(err, data)=>{
+        if (err){ throw err;
+        res.end(JSON.stringify({error: `internal error`}))
+        return;
+    }
+    res.end(data)
+ })
+})
 const Port = 3000;
-required.listen(Port,'localhost',()=>console.log(`The server is running on ${Port}`));
+myserver.listen(Port,()=>{
+    console.log(`server listening on http://localhost:${Port}`);
+})
